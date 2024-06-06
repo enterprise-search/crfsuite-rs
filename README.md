ab26c973c5fdaf81b720443cd283bcda8e2defb0
Max Z <will_d_thomas+github@icloud.com>
2024-06-06 20:35:27 +0800

    fix data read

--- README.md
+++ README.md
@@ -39,6 +39,12 @@ if __name__ == '__main__':
     print(tagged)
 ```
 
+## Run
+
+```shell
+RUST_LOG=info cargo run --example train -- -p c1=0.1 -p c2=0.1 -p max_iterations=100 -p feature.possible_transitions=1 -m ner train.data -v -v
+```
+
 ## License
 
 This work is released under the MIT license. A copy of the license is provided in the [LICENSE](./LICENSE) file.
--- examples/train.rs
+++ examples/train.rs
@@ -7,16 +7,28 @@ use crfsuite::{Algorithm, Attribute, GraphicalModel, Item, Trainer};
 #[command(version)]
 #[command(propagate_version = true)]
 struct Argv {
-    #[arg(short, long, action = clap::ArgAction::Count)]
-    verbose: u8,
+    /// specify a graphical model
     #[arg(short = 't', long = "type", default_value_t = GraphicalModel::CRF1D)]
     graphical_model: GraphicalModel,
     #[arg(short, long, default_value_t = Algorithm::LBFGS)]
     algorithm: Algorithm,
-    #[arg(short, long)]
+    /// store the model to FILE
+    #[arg(short, long, value_name = "FILE")]
     model: String,
-    #[arg(short)]
+    /// split the instances into N groups; this option is useful for holdout evaluation and cross validation
+    #[arg(short = 'g', long, value_name = "N", default_value_t = 0)]
+    split: u32,
+    /// use the M-th data for holdout evaluation and the rest for training
+    #[arg(short = 'e', long, value_name = "M", default_value_t = -1)]
+    holdout: i32,
+    /// set the algorithm-specific parameter NAME to VALUE
+    #[arg(short, long, value_name = "NAME=VALUE")]
     parameters: Vec<String>,
+    /// repeat holdout evaluations for #i in {1, ..., N} groups (N-fold cross validation)
+    #[arg(short = 'x')]
+    cross_validate: bool,
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
     #[arg(required = true)]
     datasets: Vec<PathBuf>,
 }
@@ -38,21 +50,41 @@ fn main() {
         }
     });
     argv.datasets.iter().for_each(|fpath| {
+        log::info!("reading dataset from: {:?}", fpath);
         let f = File::open(fpath).expect("failed to open: {fpath}");
-        for line in std::io::BufReader::new(f)
-            .lines()
-            .flatten()
-            .filter(|s| !s.is_empty())
-        {
-            if let Some((label, attrs)) = line.split_once('\t') {
-                let item: Item = attrs.split('\t').map(Attribute::from).collect();
-                trainer
-                    .append(&[item], &[label], 1)
-                    .expect("failed to append item");
+        let mut count = 0;
+        let mut items = Vec::new();
+        let mut labels = Vec::new();
+        for line in std::io::BufReader::new(f).lines() {
+            let line = line.expect("failed to read line");
+            if !line.is_empty() {
+                if let Some((label, attrs)) = line.split_once('\t') {
+                    let item: Item = attrs.split('\t').map(Attribute::from).collect();
+                    items.push(item);
+                    labels.push(label.to_string());
+                } else {
+                    log::warn!("invalid line: {line}");
+                }
             } else {
-                log::warn!("invalid line: {line}");
+                if !(labels.is_empty() || items.is_empty()) {
+                    trainer
+                        .append(&items, &labels, 1)
+                        .expect("failed to append item");
+                    count += items.len();
+                    items.clear();
+                    labels.clear();
+                }
             }
         }
+        if !(labels.is_empty() || items.is_empty()) {
+            trainer
+                        .append(&items, &labels, 1)
+                        .expect("failed to append item");
+                    count += items.len();
+                    items.clear();
+                    labels.clear();
+        }
+        log::info!("read {} items", count);
     });
     trainer.train(&argv.model, -1).expect("failed to train");
     log::info!("write model to {}", argv.model);
