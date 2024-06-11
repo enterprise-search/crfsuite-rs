8ae8f6754bf2847ccb97203f33d7c5219c9e6939
Max Z <will_d_thomas+github@icloud.com>
2024-06-11 13:07:55 +0800

    fmt

--- src/crf/crf1d/model.rs
+++ src/crf/crf1d/model.rs
@@ -28,7 +28,6 @@ pub struct Feature {
     pub weight: f64,
 }
 
-
 pub struct Crf1dModel {
     buffer: Vec<u8>,
     attr_refs: Vec<FeatRefs>,
@@ -59,21 +58,27 @@ impl Crf1dModel {
         let off_attrs = v[5];
         let off_label_refs = v[6];
         let off_attr_refs = v[7];
-        println!("sz: {size}, n_feats: {:?}, num_labels: {:?}, n_attrs: {:?} off_feats: {off_feats}, o_l: {off_labels}, o_a: {off_attrs}, o_l_r: {off_label_refs}, o_a_r: {off_attr_refs}", n_feats, n_labels, n_attrs);        
+        println!("sz: {size}, n_feats: {:?}, num_labels: {:?}, n_attrs: {:?} off_feats: {off_feats}, o_l: {off_labels}, o_a: {off_attrs}, o_l_r: {off_label_refs}, o_a_r: {off_attr_refs}", n_feats, n_labels, n_attrs);
 
         const CHUNK_SIZE: usize = 12;
         const FEATURE_SIZE: usize = 20;
-        let n_active_feats = u32::from_le_bytes(buffer[off_feats+8..off_feats+12].try_into().unwrap()) as usize;
+        let n_active_feats =
+            u32::from_le_bytes(buffer[off_feats + 8..off_feats + 12].try_into().unwrap()) as usize;
         let mut features = Vec::new();
         for i in 0..n_active_feats {
             let offset = off_feats + CHUNK_SIZE + FEATURE_SIZE * i;
             if offset + 20 >= size {
-                println!("exceeding size: {size}, {}", offset+20);
+                println!("exceeding size: {size}, {}", offset + 20);
             }
-            let cat = u32::from_le_bytes(buffer[offset..offset+4].try_into().expect("failed to read cat"));
-            let src = u32::from_le_bytes(buffer[offset + 4..offset+8].try_into().unwrap());
-            let dst = u32::from_le_bytes(buffer[offset + 8..offset+12].try_into().unwrap());
-            let weight: f64 = f64::from_le_bytes(buffer[offset + 12..offset+20].try_into().unwrap());
+            let cat = u32::from_le_bytes(
+                buffer[offset..offset + 4]
+                    .try_into()
+                    .expect("failed to read cat"),
+            );
+            let src = u32::from_le_bytes(buffer[offset + 4..offset + 8].try_into().unwrap());
+            let dst = u32::from_le_bytes(buffer[offset + 8..offset + 12].try_into().unwrap());
+            let weight: f64 =
+                f64::from_le_bytes(buffer[offset + 12..offset + 20].try_into().unwrap());
             let f = Feature {
                 cat: cat as usize,
                 src: src as usize,
@@ -83,35 +88,63 @@ impl Crf1dModel {
             features.push(f);
         }
         let mut label_refs = Vec::new();
-        let n_active_label_refs = u32::from_le_bytes(buffer[off_label_refs+8..off_label_refs+12].try_into().unwrap()) as usize;
+        let n_active_label_refs = u32::from_le_bytes(
+            buffer[off_label_refs + 8..off_label_refs + 12]
+                .try_into()
+                .unwrap(),
+        ) as usize;
         println!("n: {}, n_act: {}", n_labels, n_active_label_refs);
         for i in 0..n_labels {
             let offset = off_label_refs + CHUNK_SIZE + 4 * i;
-            let offset = u32::from_le_bytes(buffer[offset..offset+4].try_into().unwrap()) as usize;
-            let n = u32::from_le_bytes(buffer[offset + 4..offset+8].try_into().unwrap()) as usize;
+            let offset =
+                u32::from_le_bytes(buffer[offset..offset + 4].try_into().unwrap()) as usize;
+            let n = u32::from_le_bytes(buffer[offset + 4..offset + 8].try_into().unwrap()) as usize;
             let v = (0..n)
                 .map(|j| {
-                    u32::from_le_bytes(buffer[offset + 8 + 4 * j..offset+8+4*j+4].try_into().unwrap()) as usize
+                    u32::from_le_bytes(
+                        buffer[offset + 8 + 4 * j..offset + 8 + 4 * j + 4]
+                            .try_into()
+                            .unwrap(),
+                    ) as usize
                 })
                 .collect::<Vec<_>>();
             label_refs.push(FeatRefs::new(n, v));
         }
         let mut attr_refs = Vec::new();
-        let n_active_attr_refs = u32::from_le_bytes(buffer[off_attr_refs+8..off_attr_refs+12].try_into().unwrap()) as usize;
+        let n_active_attr_refs = u32::from_le_bytes(
+            buffer[off_attr_refs + 8..off_attr_refs + 12]
+                .try_into()
+                .unwrap(),
+        ) as usize;
         println!("n: {}, n_act: {}", n_attrs, n_active_attr_refs);
         for i in 0..n_attrs {
             let offset = off_attr_refs + CHUNK_SIZE + 4 * i;
-            let offset = u32::from_le_bytes(buffer[offset..offset+4].try_into().unwrap()) as usize;
-            let n = u32::from_le_bytes(buffer[offset + 4..offset+8].try_into().unwrap()) as usize;
+            let offset =
+                u32::from_le_bytes(buffer[offset..offset + 4].try_into().unwrap()) as usize;
+            let n = u32::from_le_bytes(buffer[offset + 4..offset + 8].try_into().unwrap()) as usize;
             let v = (0..n)
                 .map(|j| {
-                    u32::from_le_bytes(buffer[offset + 8 + 4 * j..offset+8+4*j+4].try_into().unwrap()) as usize
+                    u32::from_le_bytes(
+                        buffer[offset + 8 + 4 * j..offset + 8 + 4 * j + 4]
+                            .try_into()
+                            .unwrap(),
+                    ) as usize
                 })
                 .collect::<Vec<_>>();
             attr_refs.push(FeatRefs::new(n, v));
         }
-        let labels: Vec<(u32, String)> = CQDB::new(&buffer[off_labels as usize..]).expect("failed to read labels").iter().map(Result::unwrap).map(|(k,v)| (k, v.to_string())).collect();
-        let attrs: Vec<(u32, String)> = CQDB::new(&buffer[off_attrs as usize..]).expect("failed to read attrs").iter().map(Result::unwrap).map(|(k,v)| (k, v.to_string())).collect();
+        let labels: Vec<(u32, String)> = CQDB::new(&buffer[off_labels as usize..])
+            .expect("failed to read labels")
+            .iter()
+            .map(Result::unwrap)
+            .map(|(k, v)| (k, v.to_string()))
+            .collect();
+        let attrs: Vec<(u32, String)> = CQDB::new(&buffer[off_attrs as usize..])
+            .expect("failed to read attrs")
+            .iter()
+            .map(Result::unwrap)
+            .map(|(k, v)| (k, v.to_string()))
+            .collect();
 
         Self {
             buffer,
