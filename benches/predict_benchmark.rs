d7779464ba1ffcf40cdf81ccec8b3c95933830c0
Max Z <will_d_thomas+github@icloud.com>
2024-06-13 09:39:50 +0800

    cleanup

--- benches/predict_benchmark.rs
+++ benches/predict_benchmark.rs
@@ -1,53 +1,83 @@
 use std::{
-    fs::File,
-    io::{BufRead, BufReader},
-    path::Path,
+    fs::File, io::{BufRead, BufReader}, iter::zip, path::Path
 };
 
-use crfsuite::{Attribute, Item, Model};
+use crfsuite::{Attribute, Item, Model, Tagger};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-fn predict(fpath: &Path) {
-    let model = "ner";
-    let model = Model::from_file(model).expect("failed to load model");
-    let mut tagger = model.tagger().expect("failed to get tagger from model");
-    let f = File::open(fpath).expect("failed to open the stream for the input data");
-    let mut items = Vec::new();
-    let mut labels = Vec::new();
-    for line in BufReader::new(f).lines() {
-        let line = line.expect("failed to read line");
-        if !line.is_empty() {
-            if let Some((label, attrs)) = line.split_once('\t') {
-                let item: Item = attrs.split('\t').map(Attribute::from).collect();
-                items.push(item);
-                labels.push(label.to_string());
-            } else {
-                log::warn!("invalid line: {line}");
-            }
-        } else {
-            if !(labels.is_empty() || items.is_empty()) {
-                let pred_labels = tagger.tag(&items).expect("failed to tag");
-                for (label, pred) in labels.iter().zip(pred_labels) {
-                    // println!("{:?}\t{:?}", label, pred);
-                }
-                items.clear();
-                labels.clear();
-            }
-        }
+#[derive(Debug, Default)]
+struct Sequence {
+    items: Vec<Item>,
+    labels: Vec<String>,
+}
+
+impl Sequence {
+    pub fn push(&mut self, item: Item, label: String) {
+        self.items.push(item);
+        self.labels.push(label);
     }
-    if !(labels.is_empty() || items.is_empty()) {
-        let pred_labels = tagger.tag(&items).expect("failed to tag");
-        for (label, pred) in labels.iter().zip(pred_labels) {
-            // println!("{:?}\t{:?}", label, pred);
+
+    pub fn is_empty(&self) -> bool {
+        self.items.is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.len()
+    }    
+}
+
+#[derive(Debug, Default)]
+struct Dataset(Vec<Sequence>);
+
+
+impl From<File> for Dataset {
+    fn from(value: File) -> Self {
+        let mut dataset = Dataset::default();
+        let mut sequence = Sequence::default();
+        for line in BufReader::new(value).lines() {
+            let line = line.expect("failed to read line");
+            if !line.is_empty() {
+                if let Some((label, attrs)) = line.split_once('\t') {
+                    let item: Item = attrs.split('\t').map(Attribute::from).collect();
+                    sequence.push(item, label.to_string());
+                } else {
+                    log::warn!("invalid line: {line}");
+                }
+            } else {
+                if !(sequence.is_empty()) {
+                    dataset.0.push(sequence);
+                    sequence = Sequence::default();
+                }
+            }
         }
-        items.clear();
-        labels.clear();
+        dataset
     }
 }
 
+fn predict(tagger: &mut Tagger, dataset: &Dataset) {
+    for sequence in &dataset.0 {
+        let pred_labels = tagger.tag(&sequence.items).expect("failed to tag");
+        let mut n = 0;
+        for (label, pred) in zip(&sequence.labels, pred_labels) {
+            // println!("{:?}\t{:?}", label, pred);
+            if *label != pred {
+                n += 1;
+            }
+        }
+        // eprintln!("not match: {}/{}", n, sequence.len());
+    }    
+}
+
 fn predict_benchmark(c: &mut Criterion) {
+    let model = "ner";
+    let model = Model::from_file(model).expect("failed to load model");
+    let mut tagger = model.tagger().expect("failed to get tagger from model");
+
     let fpath = Path::new("test.data");
-    c.bench_function("test", |b| b.iter(|| predict(black_box(&fpath))));
+    let f = File::open(fpath).expect("failed to open the stream for the input data");    
+    let dataset = Dataset::from(f);
+    
+    c.bench_function("test", |b| b.iter(|| predict(black_box(&mut tagger), black_box(&dataset))));
 }
 
 criterion_group!(benchmarks, predict_benchmark);
