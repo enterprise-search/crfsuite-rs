774af8c5eb22c4d147f81eb92ec882b784484c91
Max Z <will_d_thomas+github@icloud.com>
2024-06-14 17:03:05 +0800

    add save_model

--- src/crf/crf1d/model.rs
+++ src/crf/crf1d/model.rs
@@ -1,6 +1,8 @@
-use std::{convert::TryInto, fs::File, path::PathBuf};
+use std::{convert::TryInto, ffi::CStr, fs::File, path::PathBuf};
 
 use cqdb::CQDB;
+use crfsuite_sys::crfsuite_dictionary_t;
+use libc::c_char;
 use serde::{Deserialize, Serialize};
 
 use crate::{crf::model::Model, quark::Quark};
@@ -20,26 +22,130 @@ pub struct Feature {
     pub weight: f64,
 }
 
+#[repr(C)]
 #[derive(Debug, Serialize, Deserialize)]
-struct Header {
-    magic: [u8; 4],     /* File magic. */
-    size: usize,          /* File size. */
-    typ: [u8; 4],       /* Model type */
-    version: usize,       /* Version number. */
-    num_features: usize,  /* Number of features. */
-    num_labels: usize,    /* Number of labels. */
-    num_attrs: usize,     /* Number of attributes. */
-    off_features: usize,  /* Offset to features. */
-    off_labels: usize,    /* Offset to label CQDB. */
-    off_attrs: usize,     /* Offset to attribute CQDB. */
-    off_labelrefs: usize, /* Offset to label feature references. */
-    off_attrrefs: usize,  /* Offset to attribute feature references. */
+pub struct feature {
+    pub cat: i32,
+    pub src: i32,
+    pub dst: i32,
+    pub weight: f64,
+}
+
+impl From<feature> for Feature {
+    fn from(value: feature) -> Self {
+        Self { cat: value.cat as usize, src: value.src as usize, dst: value.dst as usize, weight: value.weight }
+    }
+}
+
+#[repr(C)]
+pub struct feature_refs {
+    num_features: i32,
+    fids: * const i32,
+}
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct T {
+    num_features: usize,
+    features: Vec<Feature>,
+    labels: Vec<String>,
+    attrs: Vec<String>,
+    attr_refs: Vec<FeatRefs>,
+    label_refs: Vec<FeatRefs>,
+}
+
+const FT_STATE: i32 = 0;
+
+#[no_mangle]
+pub unsafe extern "C" fn save_model_r(fpath: * const c_char, w: * const f64, 
+    attrs: * const crfsuite_dictionary_t,
+    labels: * const crfsuite_dictionary_t,
+     L: usize, A: usize, K: usize,
+     features: * const feature,
+     attr_refs: * const feature_refs,
+     label_refs: * const feature_refs
+    ) {
+    log::info!("writing model: {fpath:?}");
+    let mut fmap: Vec<i32> = vec![-1; K];
+    let mut amap: Vec<i32> = vec![-1; A];
+
+    let mut J = 0;
+    let mut B = 0;
+    let mut t = T{ num_features: K, ..Default::default() };
+    for k in 0..K {
+        let pw = w.offset(k as isize);
+        if *pw != 0.0 {
+            fmap[k] = J;
+            J += 1;
+            let f = features.offset(k as isize).as_ref().expect("failed to read feature");
+            let mut src = f.src;
+            if f.cat == FT_STATE {
+                if amap[f.src as usize] < 0 {
+                    amap[f.src as usize] = B;
+                    B += 1;
+                    src = amap[f.src as usize];
+                }
+            }
+            let f = Feature { cat: f.cat as usize, src: src as usize, dst: f.dst as usize, weight: *pw };
+            t.features.push(f);
+        }
+    }
+    log::info!("features: {J}/{K}, attrs: {B}/{A}, labels: {L}");
+    for l in 0..L {
+        let mut label: *mut libc::c_char = std::ptr::null_mut();
+        let ret = (*labels)
+            .to_string
+            .map(|f| f(labels as *mut _, l as i32, &mut label as *mut *mut _ as *mut *const _))
+            .expect("failed to read label");
+        if ret != 0 || label.is_null() {
+            log::error!("failed to read label");
+        }
+        let label = CStr::from_ptr(label).to_str().expect("failed to read label").to_string();
+        t.labels.push(label);
+    }
+
+    for a in 0..A {
+        if amap[a] >= 0 {
+            let mut attr: *mut libc::c_char = std::ptr::null_mut();
+        let ret = (*attrs)
+            .to_string
+            .map(|f| f(attrs as *mut _, a as i32, &mut attr as *mut *mut _ as *mut *const _))
+            .expect("failed to read label");
+        if ret != 0 || attr.is_null() {
+            log::error!("failed to read label");
+        }
+        let attr = CStr::from_ptr(attr).to_str().expect("failed to read label").to_string();
+        t.attrs.push(attr);
+        }
+    }
+
+    for l in 0..L {
+        let r = label_refs.offset(l as isize).as_ref().expect("failed to read label ref");
+        let r: FeatRefs = (0..r.num_features).map(|x| fmap[*r.fids.offset(x as isize) as usize]).filter(|fid| *fid >= 0).map(|fid| fid as usize).collect();
+        t.label_refs.push(r);
+    }
+
+    t.attr_refs.resize(B as usize, FeatRefs::default());
+    for a in 0..A {
+        if amap[a] >= 0 {
+            let r = attr_refs.offset(a as isize).as_ref().expect("failed to read attr ref");
+            let id = amap[a];
+            let r: FeatRefs = (0..r.num_features).map(|x| fmap[*r.fids.offset(x as isize) as usize]).filter(|fid| *fid >= 0).map(|fid| fid as usize).collect();
+            t.attr_refs[id as usize] = r;
+        }
+    }
+    let path = CStr::from_ptr(fpath).to_str().expect("failed to read filename").to_string();
+    let w = File::create(path + ".json").expect("failed to create file");
+    serde_json::to_writer_pretty(w, &t).expect("failed to write");
+}
+
+impl From<T> for Crf1dModel {
+    fn from(value: T) -> Self {
+        Self { attr_refs: value.attr_refs, label_refs: value.label_refs, features: value.features, labels: Quark::new(&value.labels), attrs: Quark::new(&value.attrs) }
+    }
 }
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Crf1dModel {
-    header: Header,
-    buffer: Vec<u8>,
     attr_refs: Vec<FeatRefs>,
     label_refs: Vec<FeatRefs>,
     features: Vec<Feature>,
@@ -48,6 +154,12 @@ pub struct Crf1dModel {
 }
 
 impl Crf1dModel {
+    pub fn from_json(path: PathBuf) -> Self {
+        let f = File::open(path).expect("failed to open file");
+        let t: T = serde_json::from_reader(f).expect("failed to read model");
+        Self::from(t)
+    }
+    
     pub fn from_path(path: PathBuf) -> Self {
         Self::from_memory(std::fs::read(path).expect("failed to read model"))
     }
@@ -68,21 +180,6 @@ impl Crf1dModel {
         let off_attrs = v[5];
         let off_label_refs = v[6];
         let off_attr_refs = v[7];
-
-        let header = Header {
-            magic: [0; 4],
-            size: size,
-            typ: [1;4],
-            version: 1,
-            num_features: n_feats,
-            num_labels: n_labels,
-            num_attrs: n_attrs,
-            off_features: off_feats,
-            off_labels: off_labels,
-            off_attrs: off_attrs,
-            off_labelrefs: off_label_refs,
-            off_attrrefs: off_attr_refs,
-        };
 
         const CHUNK_SIZE: usize = 12;
         const FEATURE_SIZE: usize = 20;
@@ -140,8 +237,8 @@ impl Crf1dModel {
                 .try_into()
                 .unwrap(),
         ) as usize;
-        println!("n: {}, n_act: {}", n_attrs, n_active_attr_refs);
-        for i in 0..n_attrs {
+        println!("n: {}, n_act: {}", n_attrs, n_attrs);
+        for i in 0..n_active_attr_refs {
             let offset = off_attr_refs + CHUNK_SIZE + 4 * i;
             let offset =
                 u32::from_le_bytes(buffer[offset..offset + 4].try_into().unwrap()) as usize;
@@ -171,13 +268,11 @@ impl Crf1dModel {
             .collect();
 
         Self {
-            buffer,
             attr_refs,
             label_refs,
             features: features,
             labels: labels.into(),
             attrs: attrs.into(),
-            header,
         }
     }
 
@@ -261,27 +356,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn load() {
-        let path = "ner";
-        let model = Crf1dModel::from_path(path.into());
-        let header = &model.header;
-        assert_eq!(137415, header.num_features);
-        assert_eq!(9, header.num_labels);
-        assert_eq!(22033, header.num_attrs);
-
-        assert_eq!(48, header.off_features);
-        assert_eq!(583440, header.off_labels);
-        assert_eq!(585816, header.off_attrs);
-        assert_eq!(1624284, header.off_labelrefs);
-        assert_eq!(1624700, header.off_attrrefs);
-
-        assert_eq!(header.num_attrs, model.num_attrs());
-        assert_eq!(header.num_labels, model.num_labels());
-
-        assert_eq!(29169, model.features.len());
-
-        assert_eq!(header.num_attrs, model.attr_refs.len());
-        assert_eq!(header.num_labels, model.label_refs.len());
+    fn json() {
+        let path = "ner.json";
+        let f = File::open(path).expect("failed to open file");
+        let t: T = serde_json::from_reader(f).expect("failed to parse model");
+        let model = Crf1dModel::from(t);
     }
 
     #[test]
